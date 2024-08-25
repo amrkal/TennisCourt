59275a4dd924fed6d9f5899f1314fb219dc52ad0
AmrKal <a.k251198@hotmail.com>
2024-08-25 16:48:16 +0300

    Add CORS support

--- tennis-reservation/frontend/src/LoginPage.js
+++ tennis-reservation/frontend/src/LoginPage.js
@@ -9,7 +9,7 @@ function LoginPage() {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/login', { username, password })
+    axios.post('https://tenniscourt-eb61.onrender.com/api/login', { username, password })
       .then(response => {
         localStorage.setItem('access_token', response.data.access_token);
         // Redirect or handle login success
